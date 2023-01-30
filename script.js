3d4cdec45480c8baf2c31ff51ae83142fe42c3db
Shakatik <kiberking11@gmail.com>
2023-01-30 22:10:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const switchers = document.querySelectorAll(".switch__tab");
 
   const tabsin = document.querySelectorAll(".tabs__inner");
-  console.log(tabsin)
+  function perebor () {
+    switchers.forEach((element, index) => {
+     tabsin[index].classList.remove("tab__active")
+        element.classList.remove("active");
+    });
+    console.log(switcher)
+    tabsin[index].classList.add("tab__active")
+    switcher.classList.add("active");
+  }
+  switchers.forEach((switcher, index) => {
 
-
-  switchers.forEach((switcher) => {
-    tabsin.forEach((tabview) => {
-    switcher.addEventListener('click',  () => {
-
-      switchers.forEach((element) => {
-        element.classList.remove('active');
-      })
-      switcher.classList.add("active"); 
-    })
-  })
+      switcher.addEventListener("click", perebor );
+      
   });
 });
 
-
-
-
-
-
-
-
-// }
+// }D
 
 // function showSlides(n) {
 //   let i;
