a66981c049b1eea4ed018a41302085b73573cd88
Shakatik <kiberking11@gmail.com>
2023-02-13 20:31:19 +0200

    Update grib.mjs

--- grib.mjs
+++ grib.mjs
@@ -14,22 +14,12 @@ export function getGribCorrectEnding(x) {
   } else if (gribnoy>=2 && gribnoy<=4){
       // grib = 'гриба'  
       return 'END_2'    
-  }  else if (gribnoy>=5 && gribnoys<=20 || x === 11 || gribnoys == 0){
+  }  else if (gribnoys>=5 && gribnoys<=20 || gribnoys == 0 || gribnoy == 0){
       // grib = 'грибов'  
       return 'END_3'    
   }  
 }
 
-
-//  function getEndingByKey(key) {
-//   if (key === 'END_1') {
-//       return 'гриб'
-//   } else if (key === 'END_2') {
-//       return 'гриба'
-//   } else if (key === 'END_3'){
-//       return 'грибов'
-//   }
-// }
 
 const ara = {
   END_1: 'гриб',
@@ -40,54 +30,70 @@ const ara = {
 
 
 
+export function getParity(x) {
+  const key =  getCount(x);
+  const res = Endings[key]
+  return res
+}
+
 function getCount(x) {
-  const key =  getVarious(x);
-
-  // const res = ;
+return x % 2 == 0 ? 'END1' : 'END2';
 }
 
-function getVarious(x) {
-
-x % 2 == 0 ? 'END1' : 'END2';
-console.log(x)
+const Endings = {
+  END1: 'нечетное',
+  END2: 'четное',
 }
 
 
 
 
 
-console.log(getCount(5))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let calculator = {
-  sum() {
-    return this.a + this.b;
-  },
-
-  mul() {
-    return this.a * this.b;
-  },
-  read () {
-    this.a = +prompt('Введите а:', 0)
-    this.b = +prompt('Введите b:', 0)
-  }
+export function getBoxQuantity (x) {
+  const key = getBoxType (x);
+  const res = boxEnd[key];
+  return res
 }
-calculator.read();
-alert( calculator.sum() );
-alert( calculator.mul() );
+
+function getBoxType (x) {
+  const boxes = x % 10;
+  if (boxes == 1 || boxes == 0){
+      
+      return 'END_1'  
+  } else if (boxes == 2 || boxes == 3){
+      
+      return 'END_2'    
+  }  else if (boxes == 4 || boxes == 5){
+        
+      return 'END_3'    
+  }   else if (boxes == 6 || boxes == 7){
+     
+    return 'END_4'    
+}   else if (boxes == 8 || boxes == 9){
+   
+  return 'END_5'    
+}  
+
+}
+
+const boxEnd = {
+  END_1: '1 коробка',
+  END_2: '2 коробки',
+  END_3: '3 коробки',
+  END_4: '4 коробки',
+  END_5: '5 коробок',
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
