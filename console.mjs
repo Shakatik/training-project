684ce3dc52a8baaf8d3a914073c5345dfb3b334b
Shakatik <kiberking11@gmail.com>
2023-02-17 17:37:09 +0200

    Update console.mjs

--- console.mjs
+++ console.mjs
@@ -1,10 +1,10 @@
 // import {getGribCorrectEnding} from './grib.mjs'
 // import {getParity} from './grib.mjs'
 // import {getBoxQuantity} from './grib.mjs'
-import {CallFunc} from './grib.mjs'
+// import {CallFunc} from './grib.mjs'
 // console.log(getGribCorrectEnding(1112))
 // console.log(getParity(5))
 // console.log(getBoxQuantity(5))
 
 
-console.log(CallFunc('FUNC2', 10))
+// console.log(CallFunc('FUNC2', 10))
