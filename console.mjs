905352ea3e3c4fb02c28f6a5c5039c4539001a2d
Shakatik <kiberking11@gmail.com>
2023-02-23 18:10:04 +0200

    Update console.mjs

--- console.mjs
+++ console.mjs
@@ -1,32 +1,6 @@
-// import {getGribCorrectEnding} from './grib.mjs'
-// import {getParity} from './grib.mjs'
-// import {getBoxQuantity} from './grib.mjs'
-import { CallFunc } from "./grib.mjs";
-// console.log(getGribCorrectEnding(1112))
-// console.log(getParity(5))
-// console.log(getBoxQuantity(5))
 
-const select = document.getElementById("select");
-const button = document.getElementById("grib__button");
+import { Calculation } from "./grib.mjs";
 
-button.addEventListener("click", function () {
-  const input = document.getElementById("input1").value;
 
-  if (select.value == 1) {
-    const elem = input + ": " + CallFunc("FUNC1", input);
-    const p = document.createElement("p");
-    p.textContent = `${elem}`;
-    document.body.appendChild(p);
-  } else if (select.value == 2) {
-    const elem = input + ": " + CallFunc("FUNC2", input);
-    const p = document.createElement("p");
-    p.textContent = `${elem}`;
-    document.body.appendChild(p);
-  } else if (select.value == 3) {
-    const elem = input + ": " + CallFunc('FUNC3', input);
-    const p = document.createElement("p");
-    p.textContent = `${elem}`;
-    document.body.appendChild(p);
-    } 
-});
-// console.log(CallFunc('FUNC2', 10))
+Calculation()
+
