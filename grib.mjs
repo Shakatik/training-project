887b180a6ed4b49ff3e41acb8d062979a0244078
Shakatik <kiberking11@gmail.com>
2023-02-23 18:10:06 +0200

    Update grib.mjs

--- grib.mjs
+++ grib.mjs
@@ -1,110 +1,51 @@
-function getGribCorrectEnding(x) {
+export function getGribCorrectEnding(x) {
   const key = getCorrectEnding(x);
-  const res = ara[key];
-  return res
-  } 
+  const res = GRIB_KEY_COLLECTION[key];
+  return res;
+}
 
-
- function getCorrectEnding(x) {
+function getCorrectEnding(x) {
   const gribnoy = x % 10;
   const gribnoys = x % 100;
-  if (gribnoy == 1){
-      // grib = 'гриб'  
-      return 'END_1'  
-  } else if (gribnoy>=2 && gribnoy<=4){
-      // grib = 'гриба'  
-      return 'END_2'    
-  }  else if (gribnoys>=5 && gribnoys<=20 || gribnoys == 0 || gribnoy == 0){
-      // grib = 'грибов'  
-      return 'END_3'    
-  }  
+  if (gribnoy == 1) {
+    // grib = 'гриб'
+    return "END_1";
+  } else if (gribnoy >= 2 && gribnoy <= 4) {
+    // grib = 'гриба'
+    return "END_2";
+  } else if (
+    (gribnoys >= 5 && gribnoys <= 20) ||
+    gribnoys == 0 ||
+    gribnoy == 0
+  ) {
+    // grib = 'грибов'
+    return "END_3";
+  }
 }
 
+const GRIB_KEY_COLLECTION = {
+  END_1: "гриб",
+  END_2: "гриба",
+  END_3: "грибов",
+};
 
-const ara = {
-  END_1: 'гриб',
-  END_2: 'гриба',
-  END_3: 'грибов',
+export function getParity(x) {
+  const key = getCount(x);
+  const res = Endings[key];
+  return res;
 }
 
-
-
-
- function getParity(x) {
-  const key =  getCount(x);
-  const res = Endings[key]
-  return res
-}
-
-function getCount(x) {
-return x % 2 == 0 ? 'END1' : 'END2';
+export function getCount(x) {
+  return x % 2 == 0 ? "END1" : "END2";
 }
 
 const Endings = {
-  END1: 'четное',
-  END2: 'нечетное',
-}
+  END1: "четное",
+  END2: "нечетное",
+};
 
-
-
-
-
-
- function getBoxQuantity (x) {
-  const key = getBoxType (x);
-  const res = boxEnd[key];
-  return res
-}
-
-function getBoxType (x) {
-  const boxes = x % 10;
-  if (boxes == 1 || boxes == 0){
-      
-      return 'END_1'  
-  } else if (boxes == 2 || boxes == 3){
-      
-      return 'END_2'    
-  }  else if (boxes == 4 || boxes == 5){
-        
-      return 'END_3'    
-  }   else if (boxes == 6 || boxes == 7){
-     
-    return 'END_4'    
-}   else if (boxes == 8 || boxes == 9){
-   
-  return 'END_5'    
-}  
-
-}
-
-const boxEnd = {
-  END_1: '1 коробка',
-  END_2: '2 коробки',
-  END_3: '3 коробки',
-  END_4: '4 коробки',
-  END_5: '5 коробок',
-}
-
-
-
-
-export function CallFunc (key, x) {
+export function CallFunc(key, x) {
   const fun = ProcessKey[key];
-  const res = fun(x); 
-  return res
+  const res = fun(x);
+  return res;
 }
-
-
-const ProcessKey = {
-  FUNC1: getGribCorrectEnding,
-  FUNC2: getParity,
-  FUNC3: getBoxQuantity,
-}
-
-
-
-
-
-
-
-
