b15cdffc6b466f56f7f2f10d31f9d36388c5ca02
Shakatik <kiberking11@gmail.com>
2023-02-24 20:19:50 +0200

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -7,26 +7,15 @@ const ProcessKey = {
   FUNC3: getBoxQuantity,
 };
 
-const select = document.getElementById("select");
+
 const button = document.getElementById("grib__button");
 
 button.addEventListener("click", function Calculation() {
+  const select = document.getElementById("select").value;
   const input = document.getElementById("input1").value;
-  if (select.value == 1) {
-    const elem = ProcessKey['FUNC1'];
-    const res = input + ": " + elem (input);
-    const p = document.createElement("p");
-    p.textContent = `${res}`;
-    document.body.appendChild(p);
-  } else if (select.value == 2) {
-    const elem = input + ": " + CallFunc("FUNC2", input);
-    const p = document.createElement("p");
-    p.textContent = `${elem}`;
-    document.body.appendChild(p);
-  } else if (select.value == 3) {
-    const elem = input + ": " + CallFunc("FUNC3", input);
-    const p = document.createElement("p");
-    p.textContent = `${elem}`;
-    document.body.appendChild(p);
-  }
+  const elem = ProcessKey[select];
+  const res = input + ": " + elem(input);
+  const p = document.createElement("p");
+  p.textContent = `${res}`;
+  document.body.appendChild(p);
 });
