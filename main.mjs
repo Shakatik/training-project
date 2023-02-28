3589879faf21ebf4c5efb80480df8366cd2f731c
Shakatik <kiberking11@gmail.com>
2023-02-28 22:58:41 +0200

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -7,15 +7,44 @@ const ProcessKey = {
   FUNC3: getBoxQuantity,
 };
 
-
 const button = document.getElementById("grib__button");
+
+// let array = [1];
+// localStorage.setItem("array", JSON.stringify(array));
+
+const addArray = JSON.parse(localStorage.getItem("array"));
+
+render();
+function render() {
+  const neP = document.querySelectorAll("p");
+  console.log(neP);
+  neP.forEach((item) => {
+    item.remove();
+  });
+
+  addArray.forEach((element) => {
+    const p = document.createElement("p");
+    p.textContent = `${element}`;
+    document.body.appendChild(p);
+  });
+}
+
+function addItem(newItem) {
+  addArray.push(newItem);
+  render();
+  localStorage.setItem("array", JSON.stringify(addArray));
+}
 
 button.addEventListener("click", function Calculation() {
   const select = document.getElementById("select").value;
   const input = document.getElementById("input1").value;
   const elem = ProcessKey[select];
   const res = input + ": " + elem(input);
-  const p = document.createElement("p");
-  p.textContent = `${res}`;
-  document.body.appendChild(p);
+  addItem(res);
+  // const newEl = array.push(`${res}`);
+
+  // p.textContent = `${addArray}`;
+  // document.body.appendChild(p);
+  // console.log(addArray);
 });
+// .
