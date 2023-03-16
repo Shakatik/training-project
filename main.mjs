9a1e14f689a3ec0ebf770f31417452b7c32a5bb3
Shakatik <kiberking11@gmail.com>
2023-03-16 23:15:56 +0200

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -9,61 +9,95 @@ const ProcessKey = {
 
 const button = document.getElementById("grib__button");
 
-// let array = [1];
+// let array = [
+//   {
+//     id:0,
+//     value: "2 гриба",
+//     type: "Grib",
+//     },
+// ];
 // localStorage.setItem("array", JSON.stringify(array));
 
-const addArray = JSON.parse(localStorage.getItem("array"));
-const taskList = document.querySelector(".taskslist");
-console.log(taskList);
+// const addArray = JSON.parse(localStorage.getItem("array"));
+// const taskList = document.querySelector(".taskslist");
+// console.log(taskList);
 
-render();
-function render() {
-  const neP = document.querySelectorAll("li");
-  console.log(neP);
-  neP.forEach((item) => {
-    item.remove();
-  });
-  console.log(addArray);
-  addArray.forEach((element) => {
-    const li = document.createElement("li");
-    const div = document.createElement("div");
-    const deleteButton = document.createElement("button");
-    deleteButton.appendChild(document.createTextNode("Delete"));
-    div.appendChild(deleteButton) 
-    li.textContent = `${element}`;
-    li.append(div)
-    deleteButton.addEventListener("click", function removeItem(item) {
-      const parent = deleteButton.closest('li');
-      console.log(parent);
-      parent.remove();
-    });
-    document.body.appendChild(li);
+// render();
+// function render() {
+//   const neP = document.querySelectorAll("li");
+//   neP.forEach((item) => {
+//     item.remove();
+//   });
+//   console.log(addArray);
+//   addArray.forEach((element) => {
+//     const li = document.createElement("li");
+//     const div = document.createElement("div");
+//     const deleteButton = document.createElement("button");
+//     deleteButton.appendChild(document.createTextNode("Delete"));
+//     div.appendChild(deleteButton);
+//     li.textContent = `${element}`;
+//     console.log(element);
+//     li.append(div);
+//     deleteButton.addEventListener("click", function removeItem() {
+//       const parent = deleteButton.closest("li");
+//       const index = addArray.indexOf(parent.textContent, 0);
+//       console.log(index);
 
+//       if (index == -1) {
+//         addArray.splice(index, 1);
+//       }
+//       console.log(parent);
+//       parent.remove();
+//       localStorage.setItem("array", JSON.stringify(addArray));
+//     });
+//     document.body.appendChild(li);
+//     console.log(addArray);
+//   });
+// }
 
-  //   const htmlAdd = 
-  //   `<li>
-  //   <p>${element}</p>
-  //   <div>
-  //   <button class="deleteButton">X</button>
-  //   </div>
-  // </li>`;
+// function addItem(newItem) {
+//   addArray.push(newItem);
+//   render();
+//   localStorage.setItem("array", JSON.stringify(addArray));
+// }
 
-    // taskList.insertAdjacentHTML("afterend", htmlAdd);
-  });
-}
+// button.addEventListener("click", function Calculation() {
+//   const select = document.getElementById("select").value;
+//   const input = document.getElementById("input1").value;
+//   const elem = ProcessKey[select];
+//   const res = input + ": " + elem(input);
+//   addItem(res);
+// });
 
-function addItem(newItem) {
-  addArray.push(newItem);
-  render();
-  localStorage.setItem("array", JSON.stringify(addArray));
-}
+let array = [
 
+];
+localStorage.setItem("array", JSON.stringify(array));
+console.log(array)
+let objectCounter = 0;
 button.addEventListener("click", function Calculation() {
   const select = document.getElementById("select").value;
   const input = document.getElementById("input1").value;
   const elem = ProcessKey[select];
   const res = input + ": " + elem(input);
-  addItem(res);
+  const li = document.createElement("li");
+  li.textContent = res;
+  li.id = "object" + objectCounter;
+  document.body.appendChild(li);
+
+  const myObject = {
+    Id: objectCounter++,
+    value: input,
+    type: select,
+  };
+  console.log(myObject);
+  console.log(array)
+
 });
+
+
+
+
+
 
 
